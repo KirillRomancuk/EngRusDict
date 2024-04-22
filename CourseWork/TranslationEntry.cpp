43511d3576cf85f49eba14aa77242030bcb0a55c
Kirill <139076679+KirillRomancuk@users.noreply.github.com>
2024-04-22 17:16:16 +0300

    Made initial commit for TranslationEntry

---
+++ CourseWork/TranslationEntry.cpp
@@ -0,0 +1,1 @@
+#include "TranslationEntry.h"
---
+++ CourseWork/TranslationEntry.h
@@ -0,0 +1,11 @@
+#ifndef TRANSLATIONENTRY_H
+#define TRANSLATIONENTRY_H
+
+#include "AVLTree.h"
+
+struct TranslationEntry
+{
+
+}; 
+
+#endif // !TRANSLATIONENTRY_H
